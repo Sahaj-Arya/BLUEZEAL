f087596bc7bf2c97c209e0e5141178aac8d27b76
Sahaj-Arya <sahajarya9@gmail.com>
2021-04-24 02:56:22 +0530

    Updates

---
+++ precache-manifest.dd7f96cf5a6bcf20baadec80201e69c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76538c20b6fca4bb394eca185cc6739b",
+    "url": "/VIDEO/index.html"
+  },
+  {
+    "revision": "a2c3a2f0c19fd49f1f09",
+    "url": "/VIDEO/static/css/2.7679ac31.chunk.css"
+  },
+  {
+    "revision": "f0dbef9344c531c0ab21",
+    "url": "/VIDEO/static/css/main.2a3e2305.chunk.css"
+  },
+  {
+    "revision": "a2c3a2f0c19fd49f1f09",
+    "url": "/VIDEO/static/js/2.3e9a25a0.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/VIDEO/static/js/2.3e9a25a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0dbef9344c531c0ab21",
+    "url": "/VIDEO/static/js/main.789d5623.chunk.js"
+  },
+  {
+    "revision": "1de175d15069793e8b0d",
+    "url": "/VIDEO/static/js/runtime-main.dc04281b.js"
+  }
+]);
